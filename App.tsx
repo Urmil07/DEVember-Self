b4999bb917eccb46ec079e9eb7b115de0112fba4
Urmil07 <urmil.graphionic@gmail.com>
2024-02-06 15:26:39 +0530

    Advent calender list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import DayListItem from "./src/components/core/DayListItem";
 
 export default function App() {
+  const days = Array.from(Array(24).keys());
+  // .map((day, index) => index);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <SafeAreaView />
+      <FlatList
+        data={days}
+        renderItem={({ item, index }) => <DayListItem day={item} />}
+        columnWrapperStyle={styles.column}
+        contentContainerStyle={styles.content}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    gap: 10,
   },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: { gap: 10 },
 });
